909c044b29a2c1f5a63bf59ac3268d2d969d0aac
caing <crglh25@gmail.com>
2025-04-15 22:53:36 +0700

    addpost animal to test data

--- controllers/animalsController.js
+++ controllers/animalsController.js
@@ -21,4 +21,35 @@ const getAnAnimal = async (req, res) => {
     res.status(500).json({ message: "Lỗi lấy dữ liệu" });
   }
 };
-module.exports = { getAnimals, getAnAnimal };
+const addAnimal = async (req, res) => {
+  try {
+    const {
+      animalName,
+      subtitle,
+      videoDescription,
+      animalInfoHtml,
+      threatInfoHtml,
+      hunted,
+      rescueData,
+      sponsorImages,
+    } = req.body;
+
+    const newAnimal = new Animal({
+      animalName,
+      subtitle,
+      videoDescription,
+      animalInfoHtml,
+      threatInfoHtml,
+      hunted,
+      rescueData,
+      sponsorImages,
+    });
+
+    const savedAnimal = await newAnimal.save();
+    res.status(201).json(savedAnimal);
+  } catch (error) {
+    console.error("Error adding animal:", error);
+    res.status(500).json({ message: "Failed to add animal", error });
+  }
+};
+module.exports = { getAnimals, getAnAnimal, addAnimal };
--- routes/animalsRoute.js
+++ routes/animalsRoute.js
@@ -1,7 +1,12 @@
 const express = require("express");
-const { getAnAnimal, getAnimals } = require("../controllers/animalsController");
+const {
+  getAnAnimal,
+  getAnimals,
+  addAnimal,
+} = require("../controllers/animalsController");
 const router = express.Router();
 
 router.get("/", getAnimals);
 router.get("/:id", getAnAnimal);
+router.post("/add", addAnimal);
 module.exports = router;
